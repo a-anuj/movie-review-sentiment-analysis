81bd79b25228ae9cb8de767fac4b0bbae5fa0b78
a-anuj <anujamirthalingam5@gmail.com>
2025-06-14 15:05:19 +0530

    made a .py version of the code to use streamlit

---
+++ sentiment-review.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+import pandas as pd
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+import tensorflow as tf
+import numpy as np
+import streamlit as st
+from tensorflow.keras.layers import Normalization,Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.losses import SparseCategoricalCrossentropy,BinaryCrossentropy
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import CountVectorizer
+import streamlit as st
+
+
+
+df = pd.read_csv("IMDB Dataset.csv")
+print(df.head())
+
+lable_encoder = LabelEncoder()
+y_train = lable_encoder.fit_transform(df["sentiment"])
+print(y_train[:10])
+
+vec = CountVectorizer(max_features=5000, stop_words='english')
+x_train = vec.fit_transform(df["review"]).toarray()
+
+
+model = Sequential(
+    [
+        Dense(units=64,activation="relu"),
+        Dense(units=32,activation="relu"),
+        Dense(units=1,activation="sigmoid")
+    ]
+)
+
+model.compile(
+    loss = BinaryCrossentropy,
+    optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
+)
+
+model.fit(x_train,y_train,epochs=5)
+
+
+x_test = ["I will say it as a boring movie of all time "]
+x_test_vec = vec.transform(x_test).toarray()
+print(x_test_vec)
+
+
+
+
+
+prediction = model.predict(x_test_vec)
+print(prediction)
+
+
+
+
